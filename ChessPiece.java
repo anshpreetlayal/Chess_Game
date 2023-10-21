c89c1fe32c437c79edae79c9e11f0eb19ee16be4
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-10-21 19:25:50 -0400

    chessGame started...

--- ChessPiece.java
+++ ChessPiece.java
@@ -1,18 +1,12 @@
 public abstract class ChessPiece {
     private String name;
     private boolean isWhite;
-    private  boolean isBlack;
 
     public ChessPiece(String name, boolean isWhite) {
         this.name = name;
         this.isWhite = isWhite;
     }
+
     public boolean isWhite() {
         return isWhite;
     }
-    public String getName() {
-        return name;
-    }
-
-    public abstract boolean isValidMove(Cell start, Cell end);
-}
---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        // Your code to initialize and start the chess game can go here
+    }
+}
