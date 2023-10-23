12d7cb169a7c934537568e38c1d8fdfa57a18b7a
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-10-22 21:57:57 -0400

    chessGame started...

--- ChessGame.java
+++ ChessGame.java
@@ -15,9 +15,7 @@ public class ChessGame {
         boolean isGameOver = false;
         Scanner scanner = new Scanner(System.in);
         while (!isGameOver) {
-            // Display the current state of the board
             chessBoard.displayBoard();
 
-            // Get the player's move input
             System.out.print(currentPlayer.getName() + "'s turn. Enter your move (e.g., 'a2 a4'): ");
             String move = scanner.nextLine();
