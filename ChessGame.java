0bfc4ce381431462b8d12cd6728cb2dd69e03c06
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-10 19:25:09 -0500

    chessGame started...

--- ChessGame.java
+++ ChessGame.java
@@ -7,6 +7,15 @@ class Piece {
     private boolean isWhite;
 
     public Piece(PieceType type, boolean isWhite){
+         this.type = type;
+         this.isWhite = isWhite;
+    }
 
+    public PieceType getType(){
+        return type;
+    }
+
+    public boolean isWhite(){
+        return isWhite;
     }
 }
