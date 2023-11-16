77bcacfe14dabd97212581f34c32bc4562ec70e4
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-15 20:05:18 -0500

    chessGame started...

--- ChessGame.java
+++ ChessGame.java
@@ -2,20 +2,3 @@ enum PieceType {
     PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING
 }
 
-class Piece {
-    private PieceType type;
-    private boolean isWhite;
-
-    public Piece(PieceType type, boolean isWhite){
-         this.type = type;
-         this.isWhite = isWhite;
-    }
-
-    public PieceType getType(){
-        return type;
-    }
-
-    public boolean isWhite(){
-        return isWhite;
-    }
-}
