b23a13fd7ffb5f240890bbd377c90492d0015cd9
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-29 18:06:54 -0500

    chessGame started...

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,2 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChessBoard {
+    private Map<String, Spot> board;
+
+    public ChessBoard() {
+        this.board = new HashMap<>();
+        initializeBoard();
+        initializePieces();
+    }
+
+    private void initializeBoard() {
+        for (int i = 1; i <= 8; i++) {
+            for (char j = 'A'; j <= 'H'; j++) {
+                String key = Character.toString(j) + i;
+                board.put(key, new Spot(i, j));
+            }
+        }
+    }
+
+private void initializePieces(){
+
+}
 }
