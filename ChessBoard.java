c7a1d3b0e399a7acdcfbf84786f0072ba64fc5dd
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-29 18:13:39 -0500

    chessGame started...

--- ChessBoard.java
+++ ChessBoard.java
@@ -18,8 +18,42 @@ public class ChessBoard {
             }
         }
     }
+    private void initializePieces() {
+        // White pieces
+        placePiece(new Rook(true), 1, 'A');
+        placePiece(new Knight(true), 1, 'B');
+        placePiece(new Bishop(true), 1, 'C');
+        placePiece(new Queen(true), 1, 'D');
+        placePiece(new King(true), 1, 'E');
+        placePiece(new Bishop(true), 1, 'F');
+        placePiece(new Knight(true), 1, 'G');
+        placePiece(new Rook(true), 1, 'H');
+        for (char i = 'A'; i <= 'H'; i++) {
+            placePiece(new Pawn(true), 2, i);
+        }
 
-private void initializePieces(){
+        // Black pieces
+        placePiece(new Rook(false), 8, 'A');
+        placePiece(new Knight(false), 8, 'B');
+        placePiece(new Bishop(false), 8, 'C');
+        placePiece(new Queen(false), 8, 'D');
+        placePiece(new King(false), 8, 'E');
+        placePiece(new Bishop(false), 8, 'F');
+        placePiece(new Knight(false), 8, 'G');
+        placePiece(new Rook(false), 8, 'H');
+        for (char i = 'A'; i <= 'H'; i++) {
+            placePiece(new Pawn(false), 7, i);
+        }
+    }
 
-}
-}
+    public Spot getSpot(int x, char y) {
+        String key = Character.toString(y) + x;
+        return board.get(key);
+    }
+
+    public void placePiece(ChessPiece piece, int x, char y) {
+        Spot spot = getSpot(x, y);
+        if (spot != null) {
+            spot.setPiece(piece);
+        }
+    }}
