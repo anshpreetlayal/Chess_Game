9008c316d317dcdea1ebc0d0b3612f117f4f50e6
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-01-01 13:32:44 -0500

    chessGame started...

--- GridBlock.java
+++ GridBlock.java
@@ -4,31 +4,9 @@ public class GridBlock {
     private int y;
 
     public GridBlock(int x, int y, ChessPiece piece) {
-        this.setPiece(piece);
-        this.setX(x);
-        this.setY(y);
-    }
-    public ChessPiece getPiece() {
-        return this.piece;
-    }
-
-    public void setPiece(ChessPiece p) {
-        this.piece = p;
-    }
-
-    public int getX() {
-        return this.x;
-    }
-
-    public void setX(int x) {
+        this.piece = piece;
         this.x = x;
-    }
-
-    public int getY() {
-        return this.y;
-    }
-
-    public void setY(int y) {
         this.y = y;
     }
+
 }
