b62ff67920fc146b89407e05cd6cb2383c748162
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-01-12 13:46:39 -0500

    chessGame started...

--- Square.java
+++ Square.java
@@ -1,29 +1,12 @@
+// Square class represents a square on the chessboard
 public class Square {
     private int row;
     private int col;
-    private Piece piece;
 
-    public int getCol() {
-        return col;
-    }
-
-    public Piece getPiece() {
-        return piece;
-    }
-
-    public void setPiece(Piece piece) {
-        this.piece = piece;
-    }
-
-    public void setCol(int col) {
+    public Square(int row, int col) {
+        this.row = row;
         this.col = col;
     }
 
-    public int getRow() {
-        return row;
-    }
 
-    public void setRow(int row) {
-        this.row = row;
-    }
 }
