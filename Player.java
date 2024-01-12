4109cee273a9091097a1d2e225c74f31aacef806
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-01-12 13:50:16 -0500

    chessGame started...

--- Player.java
+++ Player.java
@@ -1,6 +1,12 @@
+// Player class represents a player in the chess game
 public class Player {
     private String name;
-    private Color color;
-    private List<Piece> capturedPieces;
+    private String color;
 
+    public Player(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    // Getters and setters for name and color
 }
