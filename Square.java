da3062d3bbd13bd1d69dec823b46235e24bd926b
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-01-12 13:47:11 -0500

    chessGame started...

--- Square.java
+++ Square.java
@@ -8,5 +8,19 @@ public class Square {
         this.col = col;
     }
 
+    public int getCol() {
+        return col;
+    }
 
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
 }
