02aa5ec806b1452a414a7b3c5eadac2aa13e2a9d
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-01-12 13:48:20 -0500

    chessGame started...

--- Piece.java
+++ Piece.java
@@ -1,2 +1,14 @@
 public abstract class Piece {
+    private String color;
+    private Square currentPosition;
+
+    public Piece(String color, Square currentPosition) {
+        this.color = color;
+        this.currentPosition = currentPosition;
+    }
+
+    // Abstract method to be implemented by specific piece classes
+    public abstract boolean isValidMove(Square destination);
+
+
 }
