af7c8515554f1c781d786efab5158118b47ccbe9
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-01-15 18:56:42 -0500

    modified piece class

--- King.java
+++ King.java
@@ -1,1 +1,18 @@
 
+public class King extends Piece{
+
+
+    public King(String color, Square currentPosition) {
+        super(color, currentPosition);
+    }
+
+    @Override
+    public boolean isValidMove(Square destination) {
+        return false;
+    }
+
+    @Override
+    public boolean canMove(Board board, Square start, Square end) {
+        return false;
+    }
+}
