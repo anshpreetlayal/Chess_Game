b7ddb92769450613a15ebca716140df3f76be1c0
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-01-15 19:00:59 -0500

    modified piece class

--- King.java
+++ King.java
@@ -1,9 +1,16 @@
+public class King extends Piece {
+    private boolean castlingDone = false;
 
-public class King extends Piece{
+    public King(boolean isWhite) {
+        super(isWhite);
+    }
 
+    public boolean isCastlingDone() {
+        return this.castlingDone;
+    }
 
-    public King(String color, Square currentPosition) {
-        super(color, currentPosition);
+    public void setCastlingDone(boolean castlingDone) {
+        this.castlingDone = castlingDone;
     }
 
     @Override
@@ -13,6 +20,41 @@ public class King extends Piece{
 
     @Override
     public boolean canMove(Board board, Square start, Square end) {
+        // We can't move the piece to a square that
+        // has a piece of the same color
+        if (end.getPiece() != null && end.getPiece().isWhite() == this.isWhite()) {
+            return false;
+        }
+
+        int rowDiff = Math.abs(start.getRow() - end.getRow());
+        int colDiff = Math.abs(start.getCol() - end.getCol());
+
+        if (rowDiff + colDiff == 1) {
+            // Check if this move will not result in the king
+            // being attacked, if so, return true
+            return true;
+        }
+
+        return isValidCastling(board, start, end);
+    }
+
+    private boolean isValidCastling(Board board, Square start, Square end) {
+        if (this.isCastlingDone()) {
+            return false;
+        }
+
+        // Logic for determining if castling is valid
+        // Implement based on the specific rules of chess
+
+        // Example: Check if the rook is in the correct position, if there are no pieces
+        // in the squares between the king and rook, and if the squares the king crosses
+        // during castling are not under attack.
+
+        return true;
+    }
+
+    public boolean isCastlingMove(Square start, Square end) {
+
         return false;
     }
 }
