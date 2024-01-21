daf2a2ad0ddb5ae54c45f3c814a040de72174fff
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-01-21 18:24:35 -0500

    updating chessgame

--- Game.java
+++ Game.java
@@ -1,13 +1,9 @@
+import java.util.Scanner;
+
 public class Game {
     private Board board;
+    private Player currentPlayer;
     private Player player1;
     private Player player2;
 
-    public Game(Board board, Player player1, Player player2) {
-        this.board = board;
-        this.player1 = player1;
-        this.player2 = player2;
-    }
-
-    // to be done:Methods for starting the game, taking turns, handling moves, etc.
 }
