d1ba7f1b2ec39837a800d6c1cc557e9279a65b9b
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-01-21 18:01:22 -0500

    updating chessgame

--- Knight.java
+++ Knight.java
@@ -1,5 +1,6 @@
 public class Knight extends Piece {
-    public Knight (String color, Square currentPosition) {
+
+    public Knight(String color, Square currentPosition) {
         super(color, currentPosition);
     }
 
@@ -12,4 +13,6 @@ public class Knight extends Piece {
     public boolean canMove(Board board, Square start, Square end) {
         return false;
     }
+
+
 }
